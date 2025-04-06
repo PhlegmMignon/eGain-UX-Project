7b22fd51c46f172370919b9400fcd47298e9eb21
Phlegm Mignon <dmah@ucdavis.edu>
2025-04-06 01:27:51 -0700

    baseurl typo

--- project/vite.config.js
+++ project/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/eGain-UX-project",
+  base: "/eGain-UX-Project",
   plugins: [vue(), tailwindcss()],
 });
