aecb2deeee0aa12028e9f392776d364a99261f69
Phlegm Mignon <dmah@ucdavis.edu>
2025-04-06 17:46:14 -0700

    Finished Header and NextBtn. Configured Primevue styles for radiobtn. Fix text colors

--- project/src/components/Header.vue
+++ project/src/components/Header.vue
@@ -6,7 +6,7 @@ defineProps({
 </script>
 
 <template>
-  <header class="p-8 pt-12 gap-8 flex justify-between items-center">
+  <header class="gap-8 flex justify-between items-center">
     <h1 class="text-4xl flex-grow text-center">{{ title }}</h1>
     <img :src="logo" alt="Vue Logo" class="w-10 h-10" />
   </header>
--- project/src/main.js
+++ project/src/main.js
@@ -4,13 +4,17 @@ import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import router from "./router";
 
+// import { RadioButton } from "primevue";
+
 const app = createApp(App);
 
-app.use(PrimeVue, {
-  theme: {
-    preset: "none",
-  },
-});
+// app.use(PrimeVue, {
+//   theme: {
+//     preset: "none",
+//   },
+// });
+
+app.use(PrimeVue);
 
 app.use(router);
 
